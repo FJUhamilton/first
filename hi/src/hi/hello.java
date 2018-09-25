febd618959145f3d6e47256fd80d926aff1cbfe5
user <user@es302n12>
2018-09-25 12:04:24 +0800

    hihi

---
+++ hi/src/hi/hello.java
@@ -0,0 +1,12 @@
+package hi;
+
+public class hello {
+
+	public static void main(String[] args) {
+		int age=19;
+		System.out.println(age);
+		float weight = 12.5f;
+		System.out.println(weight);
+	}
+
+}
